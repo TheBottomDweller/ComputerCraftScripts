8faf8f421f7b4876d033b3b14243639c2fdd86f4
Bottom Dweller <23364873+TheBottomDweller@users.noreply.github.com>
2021-12-12 23:13:09 -0600

    A farm script and the start of a mine script

---
+++ farm.lua
@@ -0,0 +1,34 @@
+local SLOT_COUNT = 16
+local cropChoice
+local cropSeed
+
+term.write("Please Provide Crop ID: ")
+cropChoice = read()
+term.write("Now Provide Seed ID")
+cropSeed = read()
+
+function retrieveHarvest()
+    for i = 1, 6 , 1 do
+        turtle.suck()
+    end
+end
+
+function getItemIndex(itemName)
+    for slot = 1, SLOT_COUNT, 1 do
+        local item = turtle.getItemDetail(slot)
+        if (item ~= nil) then
+            if(item["name"] == itemName) then
+                return slot
+            end
+        end
+    end
+end
+
+isBlock, data = turtle.inspect()
+
+if (data['state']['age'] == 7) then
+    turtle.dig()
+    retrieveHarvest()
+    itemIndex = getItemIndex(cropChoice)
+    seedIndex = getItemIndex(cropSeed)
+end
---
+++ mine.lua
@@ -0,0 +1,2 @@
+local SLOT_COUNT = 16
+local direction = 1
