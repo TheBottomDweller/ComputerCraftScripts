dc9eb27a446a51f446ba0659988761b1c9f1126e
Bottom Dweller <23364873+TheBottomDweller@users.noreply.github.com>
2022-01-13 20:19:31 -0600

    Update mastercomputer.lua

--- mastercomputer.lua
+++ mastercomputer.lua
@@ -34,7 +34,7 @@ function fill(text, color, bData)
     monitor.setBackgroundColor(color)
     local yspot = math.floor((bData["ymin"] + bData["ymax"]) /2)
     local xspot = math.floor(((bData["xmax"] - bData["xmin"]) - string.len(text)) / 2) + 1
-    for j == bData["ymin"], bData["ymax"] do
+    for j = bData["ymin"], bData["ymax"] do
         monitor.setCursorPos(bData["xmin"], j)
         if j == yspot then
             for k = 0, bData["xmax"] - bData["xmin"] - string.len(text) + 1 do
@@ -64,3 +64,6 @@ function screen()
         fill(name, currColor, data)
     end
 end
+
+MainMenuTable()
+screen()
