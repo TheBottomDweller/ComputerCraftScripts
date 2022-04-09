546a9c0ac03ddf2f1e2a4c2ce462cb2e754ff3bf
Bottom Dweller <23364873+TheBottomDweller@users.noreply.github.com>
2022-04-09 07:35:54 -0500

    Update twitchchat.lua

--- twitchchat.lua
+++ twitchchat.lua
@@ -1,9 +1,11 @@
 local screen = peripheral.wrap("right")
+local xpos = 1
+local ypos = 1
 screen.clear()
-screen.setCursorPos(1,1)
+screen.setCursorPos(xpos,ypos)
 while true do
     text = http.get("https://dashboard.twitch.tv/popout/u/thebottomdweller/stream-manager/chat?uuid=1cc5fef56301e3c0f6e020b752744553")
     screen.write(text)
-    curCursorPos = screen.getCursorPos(y)
-    screen.setCursorPos(1, curCursorPos+1)
+    ypos = ypos + 1
+    screen.setCursorPos(1, ypos)
 end
